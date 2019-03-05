e674c48bd9180baca7f801c2c04c0440f76014db
Adam Zaręba <adam.d.zareba@gmail.com>
2019-03-05 16:17:17 +0100

    reorganized meals, added new tv series

--- src/meals.js
+++ src/meals.js
@@ -1,8 +1,9 @@
-var meals = ['breakfast', 'lunch', 'dinner'];
-meals.push('supper');
+var meals = ['breakfast', 'lunch', 'supper'];
+
 console.log(meals.sort());
 meals.forEach(function(currentValue, index, arr){
   console.log(index, currentValue, arr);
 });
 
 var mealsCopy = meals.slice();
+var mealsSort = meals.sort();
--- src/tvSeries.js
+++ src/tvSeries.js
@@ -1,6 +1,6 @@
 var tvSeries = ['Game of Thrones', 'Stranger Things', 'Black Mirror'];
-
-console.log(tvSeries.sort());
+tvSeries.push('The Punisher', 'Lucifer');
+console.log(tvSeries.slice());
 tvSeries.forEach(function(currentValue, index, arr){
   console.log(index, currentValue, arr);
 });
